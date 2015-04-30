1c9d63b5e02dcf754aa9fff8e461e4eb937853e4
jmocana2 <telecomu4@gmail.com>
2015-04-30 13:11:21 +0200

    desktop and horizontal tablet version

--- js/main.js
+++ js/main.js
@@ -1,14 +1,18 @@
  $(document).ready(function () {
 
+ 	var mySwiper = {};
+ 	mySwiper.init = false;
+
  	function getWithNavigator(){
  		return $(window).width();
  	}
 
- 	if(getWithNavigator() >= 645){
-	 	var index = $('.swiper-slide').length;
-	 	var my_sections = ['Who', 'What', 'How', 'Where'];
-
-	 	my_swiper = new Swiper ('.swiper-container', {
+ 	//carousel tablet and desktop
+ 	function initSwiper(){
+ 		var index = $('.swiper-slide').length;
+	 	var my_sections = ['Who', 'What', 'How', 'Where']; //Config sections	 	
+	 	
+	 	mySwiper = new Swiper ('.swiper-container', {
 	      // Optional parameters
 	      direction: 'horizontal',
 	      loop: true,
@@ -20,5 +24,23 @@
 		      return '<li class="' + className + " " + my_sections[index].toLowerCase() + '"><span>' + my_sections[index] + '</span></li>';
 		  }
 	    });
-	} 		       
+
+	    mySwiper.init = true;
+ 	}
+
+ 	if(!mySwiper.init && getWithNavigator() >= 645){
+ 		initSwiper();
+ 	} 	
+
+ 	//resize window
+ 	$( window ).resize(function() {
+	  if(getWithNavigator() < 645 && mySwiper.init){
+	  	mySwiper.destroy(true, true);
+	  	mySwiper.init = false;	  	 	
+	  }	  	  
+	  if(getWithNavigator() >= 645 && !mySwiper.init){
+	    initSwiper();
+	  }
+	});
+ 	 		       
   });
